447df4aa806e03cc91b660006d443a81ae87735a
guoliang <761297884@qq.com>
2022-02-15 23:30:49 +0800

    调整示例代码目录

--- example/main.go
+++ example/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"embed"
 	"github.com/guoliang1994/installer/cmd"
+	program2 "github.com/guoliang1994/installer/example/program"
 	"github.com/guoliang1994/installer/global"
-	"github.com/guoliang1994/installer/program"
 )
 
 //go:embed lang
@@ -14,8 +14,8 @@ func main() {
 	global.LangFs = LangFs
 	description := `xyz程序描述`
 	installer := cmd.NewInstaller()
-	client := cmd.NewProgram("client", "xyz客户端", "xyz客户端描述", "v1.0.0", &program.Client{})
-	server := cmd.NewProgram("server", "xyz服务端", "xyz服务端描述", "v1.0.0", &program.Server{})
+	client := cmd.NewProgram("client", "xyz客户端", "xyz客户端描述", "v1.0.0", &program2.Client{})
+	server := cmd.NewProgram("server", "xyz服务端", "xyz服务端描述", "v1.0.0", &program2.Server{})
 	installer.AddProgram(client).
 		AddProgram(server).
 		SetRootCmd("xyz", "xyz程序", description).
